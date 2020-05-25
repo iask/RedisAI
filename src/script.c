0065a0f98acb5234bb77e47c87bacbed0c76962f
Luca Antiga <luca.antiga@orobix.com>
2020-05-26 00:00:26 +0200

    Enable chunking for large models in AOF, RDB, replication and MODELGET

--- src/script.c
+++ src/script.c
@@ -13,6 +13,7 @@
 #include "script_struct.h"
 #include "stats.h"
 #include "util/arr_rm_alloc.h"
+#include "version.h"
 
 RedisModuleType* RedisAI_ScriptType = NULL;
 
@@ -82,8 +83,8 @@ static void RAI_Script_AofRewrite(RedisModuleIO* aof, RedisModuleString* key,
                                   void* value) {
   RAI_Script* script = (RAI_Script*)value;
 
-  RedisModule_EmitAOF(aof, "AI.SCRIPTSET", "sccc", key, script->devicestr,
-                      script->tag, script->scriptdef);
+  RedisModule_EmitAOF(aof, "AI.SCRIPTSET", "scccc", key, script->devicestr,
+                      script->tag, "SOURCE", script->scriptdef);
 }
 
 static void RAI_Script_DTFree(void* value) {
@@ -105,7 +106,7 @@ int RAI_ScriptInit(RedisModuleCtx* ctx) {
                                      .digest = NULL};
 
   RedisAI_ScriptType =
-      RedisModule_CreateDataType(ctx, "AI_SCRIPT", 0, &tmScript);
+      RedisModule_CreateDataType(ctx, "AI_SCRIPT", RAI_ENC_VER_MM, &tmScript);
   return RedisAI_ScriptType != NULL;
 }
 
